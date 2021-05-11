f776857b7f0d1fd35df84019cfc529a041513b30
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2021-05-11 07:54:56 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Cloud Tech Masters"
+    return "Hello World......!!!!!"
 
 if __name__ == '__main__':
     app.run(threaded=True,host='0.0.0.0',port=5000)
